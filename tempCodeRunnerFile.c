6db16b2908349c86a2d99e62c8d9d8fdd693b85e
dominicherna <dshernandez5@miners.utep.edu>
2025-11-20 16:44:05 -0700

    Cleaned up prev change

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    printf("Enter 6 integers: ");
+    int i;
+    int arr[i];
+    for (i=0; i<6; ++i)
+    {
+        scanf("%d",&i);
+    }
+    printf("%d",&arr[i]);
+}
